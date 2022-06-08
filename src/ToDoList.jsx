623b06695eec10910e7b525b1f852dab1716fd57
triciaoliveira <oliveira.ptricia@gmail.com>
2022-06-08 19:51:06 -0300

    style: started working in component style

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,3 +1,5 @@
+import './styles/ToDoList.scss';
+import './styles/reset.css'
 const ToDoList = () => {
   return (
     <container>
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ToDoList from './App';
+import ToDoList from './ToDoList';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
