db188c889fdc72b506a769a1a82e2f6597d0b2d7
pradokez <pradokeziah@gmail.com>
2022-06-07 21:40:33 -0300

    feat: add component and basic HTML structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const ToDoList = () => {
+  return (
+    <container>
+      <div className="header">
+        <h1 className="list-title">para hoje</h1>
+        <h2 className="date-subtitle">junho | 06/06/2022</h2>
+      </div>
+      <div className="list-items">
+        <ul>
+          <li><input type="checkbox"/>teste</li>
+          <li><input type="checkbox"/>teste</li>
+          <li><input type="checkbox"/>teste</li>
+        </ul>
+      </div>
+    </container>
+  );
+};
+
+export default ToDoList;
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => (
-  <div />
-);
-
-export default App;
