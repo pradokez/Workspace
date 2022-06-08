a63a9153d0f2f4a6aeb8952a745276a61f37073c
triciaoliveira <oliveira.ptricia@gmail.com>
2022-06-08 20:36:19 -0300

    style: wip

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,8 +1,10 @@
 import '../styles/ToDoList.scss';
 import '../styles/reset.css'
+
 const ToDoList = () => {
   return (
-    <container>
+    <div className="container">
+      <div className='box'>
       <div className="header">
         <h1 className="list-title">para hoje</h1>
         <h2 className="date-subtitle">junho | 06/06/2022</h2>
@@ -13,8 +15,9 @@ const ToDoList = () => {
           <li><input type="checkbox"/>teste</li>
           <li><input type="checkbox"/>teste</li>
         </ul>
+        </div>
       </div>
-    </container>
+    </div>
   );
 };
 
